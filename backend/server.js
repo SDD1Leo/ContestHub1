dba04add0965aea6b802fbd872f306d8b5e02547
SDD1Leo <131460063+SDD1Leo@users.noreply.github.com>
2025-07-07 22:24:30 +0530

    fix: server rewrite

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
-const app = require('./app');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // ✅ Initialize contest scheduler
-const contestFetcherCron = require('./scheduler'); 
-const scheduleAllNotifications = require('./utils/scheduleNotification');
-
+const contestFetcherCron = require("./scheduler");
+const scheduleAllNotifications = require("./utils/scheduleNotification");
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose
+  .connect(MONGO_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    scheduleAllNotifications();
-    contestFetcherCron();
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+      contestFetcherCron();
+      scheduleAllNotifications();
+    });
   })
-  .catch((err) => console.error('Mongo Error:', err));
+  .catch((err) => console.error("Mongo Error:", err));
